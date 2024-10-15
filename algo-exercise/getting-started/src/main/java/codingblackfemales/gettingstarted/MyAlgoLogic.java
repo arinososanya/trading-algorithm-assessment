999c7e788552f442628523b4b0f55426d8e8b032
Arin Ososanya <arin.ososanya@gmail.com>
2024-10-15 17:34:12 +0100

    MyAlgoLogic skeleton as seen in AddCancelAlgoLogic

--- algo-exercise/getting-started/src/main/java/codingblackfemales/gettingstarted/MyAlgoLogic.java
+++ algo-exercise/getting-started/src/main/java/codingblackfemales/gettingstarted/MyAlgoLogic.java
@@ -5,54 +5,93 @@ import codingblackfemales.action.CancelChildOrder;
 import codingblackfemales.action.CreateChildOrder;
 import codingblackfemales.action.NoAction;
 import codingblackfemales.algo.AlgoLogic;
+import codingblackfemales.collection.extrusive.Map;
+import codingblackfemales.sotw.ChildOrder;
 import codingblackfemales.sotw.SimpleAlgoState;
+import codingblackfemales.sotw.marketdata.AskLevel;
 import codingblackfemales.sotw.marketdata.BidLevel;
 import codingblackfemales.util.Util;
 import messages.order.Side;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.HashMap;
+import java.util.List;
+
 public class MyAlgoLogic implements AlgoLogic { // implementing the AlgoLogic interface. This class only contains abstract methods
 
     private static final Logger logger = LoggerFactory.getLogger(MyAlgoLogic.class);
+    private static final int MAX_ORDERS = 3;
+    private static final int MAX_TOTAL_ORDERS = 10;
+    private static final long MAX_PRICE_DISTANCE_PERCENT = 1000; // I'm going to assume that 1000 ticks is one day
+    private static final long MIN_SPREAD = 2; // I will only consider creating orders if the spread is at least 2
+
 
     @Override
     public Action evaluate(SimpleAlgoState state) { // State is an instance of SimpleAlgoState i.e it's an object of type SimpleAlgoState, that's passed to the evaluate method. The evaluate method returns an Action, which can be a CancelChildOrder, CreateChildOrder, or NoAction.
-
         var orderBookAsString = Util.orderBookToString(state); // checks the current state of the order book and logs it
-
         logger.info("[MYALGO] The state of the order book is:\n" + orderBookAsString);
 
-        var totalOrderCount = state.getChildOrders().size(); // get the total number of child orders in the current state
+        var totalOrderCount = state.getChildOrders().size(); // get the total number of child orders, regardless of their current status (rejected, cancelled, filled, active(live) orders etc.)
 
-        //make sure we have an exit condition... this is a safety check for if there are more than 20 child orders, then the algo does nothing
-        if (totalOrderCount > 20) {
+        //make sure we have an exit condition... this is a safety check for if there are more than 15 child orders, then the algo does nothing
+        if (totalOrderCount > MAX_TOTAL_ORDERS) {
             return NoAction.NoAction;
         }
 
-        final var activeOrders = state.getActiveChildOrders(); // getting all the current live/active child orders
-        if (activeOrders.size() > 0) {
-            final var option = activeOrders.stream().findFirst();
+        // 1. Check if we need to cancel old orders
+        List<ChildOrder> allOrders = state.getChildOrders();
+        if (allOrders.size() > MAX_TOTAL_ORDERS) {
+            ChildOrder oldestOrder = allOrders.get(0); //assuming orders are stored in chronological order
+            logger.info("[MYALGO] Cancelling oldest order to maintain order limit");
+            return new CancelChildOrder(oldestOrder);
+        }
 
-            if (option.isPresent()) { // if there are active orders, it attempts to cancel the first one. If there's no first order (which shouldn't happen if size>0), it does nothing
-                var childOrder = option.get();
-                logger.info("[ADDCANCELALGO] Cancelling order:" + childOrder);
-                return new CancelChildOrder(childOrder);
-            }
-            else{
-                return NoAction.NoAction;
-            }
-        } else {
-            BidLevel level = state.getBidAt(0);
-            final long price = level.price;
-            final long quantity = level.quantity;
-            logger.info("[ADDCANCELALGO] Adding order for" + quantity + "@" + price);
+        // 1. Get the number of bid and ask levels
+        int bidLevels = state.getBidLevels();
+        int askLevels = state.getAskLevels();
+        logger.info("[MYALGO] Market Depth - Bid Levels: {}, Ask Levels: {}", bidLevels, askLevels);
+
+        // 2. Get the market data (best bid and ask)
+        final AskLevel askFarTouch = state.getAskAt(0);
+        final BidLevel bidNearTouch = state.getBidAt(0);
+
+        // Make sure both values are not null
+        if (askFarTouch == null || bidNearTouch == null) {
+            logger.warn("[MYALGO] Incomplete market data - unable to make decisions");
+            return NoAction.NoAction;
+        }
+
+//     If all is good, create and then calculate the spread
+        final long spread = askFarTouch.price - bidNearTouch.price;
+        if (spread < 0) {
+            logger.warn("[MYALGO] Negative spread detected: {}. Possible market anomaly or data error.", spread);
+            return NoAction.NoAction;
+        }
+        if (spread < MIN_SPREAD) {
+            logger.info("[MYALGO] Spread too small for order creation: {}", spread);
+            return NoAction.NoAction;
+        }
+
+        // So we can visually see what the best bid and ask are
+        logger.info("[MYALGO] Best Bid: {} @ {}, Best Ask: {} @ {}",
+                bidNearTouch.quantity, bidNearTouch.price,
+                askFarTouch.quantity, askFarTouch.price);
+
+        // Create order if conditions are met
+        var activeOrderCount = state.getActiveChildOrders().size();
+        if (activeOrderCount < MAX_ORDERS) {
+            // Place the buy order at the ask price to cross the spread
+            long price = askFarTouch.price;
+            long quantity = Math.min(100, askFarTouch.quantity); // I'm still limiting quantity for risk management
+            logger.info("[MYALGO] Creating new aggressive buy order: {} @ {}", quantity, price);
             return new CreateChildOrder(Side.BUY, quantity, price);
-        }
+
         }
 
-
+    return NoAction.NoAction;
     }
+}
 
 /********
  *
@@ -60,12 +99,25 @@ public class MyAlgoLogic implements AlgoLogic { // implementing the AlgoLogic in
  *
  */
 
+/*
+Overview: Adding and Cancelling
+1. Get the best bid and ask and then calculate the spread.
+I also want to log the number of bid and ask levels for a broader view of market depth.
+
+2. I want to allow for up to 3 multiple active orders at different levels below the current market price
+because if the price drops significantly, I might fill multiple orders and acquire a larger position at a good average price.
+
+
 
  /*
-        Mine: Get all active orders
+        Mine:
+        Evaluate the current market state using SimpleAlgoState state method
+
+
+        Get current active orders
       var activeOrders = state.getActiveChildOrders();
 
-        // If I have active orders, cancel the oldest one. Oldest one = > 10 ticks
+        // If I have active orders, cancel the old or oldest one. Oldest one = > 10 ticks
         if(!activeOrders.isEmpty()){
 
         }
@@ -76,10 +128,11 @@ public class MyAlgoLogic implements AlgoLogic { // implementing the AlgoLogic in
         // If we didn't cancel or create an order, do nothing
         return NoAction.NoAction;*/
 
-        /* To make profit: Sell the asset for more than I bought it for. The smaller the spread,
+        /* To make profit: I want to sell the asset for more than I bought it for. The smaller the spread,
         the quicker you might be able to make a profit, but potentially a smaller one.
         create a buy and sell threshold in your logic with your
         algo only buying when your price is at that threshold or below it
         and selling when it reaches your sell threshold or exceeds it.
+        We could use the new trade method in SimpleAlgoState to analyse this.
         *
         * */
--- algo-exercise/getting-started/src/test/java/codingblackfemales/gettingstarted/MyAlgoTest.java
+++ algo-exercise/getting-started/src/test/java/codingblackfemales/gettingstarted/MyAlgoTest.java
@@ -3,7 +3,14 @@ package codingblackfemales.gettingstarted;
 import codingblackfemales.algo.AlgoLogic;
 import org.junit.Test;
 
+import java.util.stream.Stream;
 
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.Assert.assertEquals;
+
+// MY NOTE:
 /**
  * This test is designed to check your algo behavior in isolation of the order book.
  *
@@ -11,7 +18,7 @@ import org.junit.Test;
  *
  * You should then add behaviour to your algo to respond to that market data by creating or cancelling child orders.
  *
- * When you are comfortable you algo does what you expect, then you can move on to creating the MyAlgoBackTest.
+ * When you are comfortable your algo does what you expect, then you can move on to creating the MyAlgoBackTest.
  *
  */
 public class MyAlgoTest extends AbstractAlgoTest {
@@ -30,6 +37,41 @@ public class MyAlgoTest extends AbstractAlgoTest {
         send(createTick());
 
         //simple assert to check we had 3 orders created
-        //assertEquals(container.getState().getChildOrders().size(), 3);
+        int expectedOrderCount = 3;
+        assertEquals("Expected 3 orders to be created", expectedOrderCount, container.getState().getChildOrders().size());
     }
+
+    @Test
+    public void testMaxOrderLimit () throws Exception{
+        // Send market data multiple times
+        for (int i = 0; i < 3; i++){
+            send(createTick());
+        }
+
+    }
+
+    //Examples of Parameterized Tests
+
+//    @DisplayName(value = "String Exercises")
+//    public class AppTest {
+//
+//        @ParameterizedTest
+//        @MethodSource
+//        @DisplayName("concatenate() method returns correctly concatenated string")
+//        public void concatenate_ReturnsConcatenatedString(String word1, String word2, String word3, String expected) {
+//            final String result = App.concatenate(word1, word2, word3);
+//
+//            assertThat(result, is(equalTo(expected)));
+//        }
+//
+//        static Stream<Arguments> concatenate_ReturnsConcatenatedString() {
+//            return Stream.of(
+//                    arguments("Red", "Green", "Blue", "RedGreenBlue"),
+//                    arguments("one", "two", "three", "onetwothree"),
+//                    arguments("QUICK", "BROWN", "FOX", "QUICKBROWNFOX"),
+//                    arguments("www.", "google.", "com", "www.google.com"),
+//                    arguments("Intro ", "to ", "Java", "Intro to Java"));
+//        }
+
+
 }
